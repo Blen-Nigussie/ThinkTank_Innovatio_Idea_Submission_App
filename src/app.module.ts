8a0cdd4c8759727a4cadf3993c585f55d7c29ad5
Derartu Nigatu Tuge <167537446+derartuge19@users.noreply.github.com>
2025-05-11 15:04:41 +0300

    Update app.module.ts
    app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,36 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { FeedbackModule } from './feedback/feedback.module';
 import { IdeasModule } from './ideas/ideas.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/entities/user.entity';
+import { Feedback } from './feedback/entities/feedback.entity';
+import { Idea } from './ideas/entities/ideas.entity';
 import { ProfileModule } from './profile/profile.module';
+import { Profile } from './profile/entities/profile.entity';
+import { profile } from 'console';
 
 @Module({
-  imports: [AuthModule, UsersModule, FeedbackModule, IdeasModule, ProfileModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'dere2010',
+      database: 'Think-Tank',
+      synchronize: true,
+      entities: [User, Feedback, Idea, Profile],
+      autoLoadEntities: true,
+    }),
+
+    TypeOrmModule.forFeature([User, Feedback, Idea, Profile]), // Import the entities here
+
+    AuthModule,
+    UsersModule,
+    FeedbackModule,
+    IdeasModule,
+    ProfileModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
