e91cf00ddac61d74d1930970016462aa204db60d
Hasset Dejene <162673489+hazmute@users.noreply.github.com>
2025-05-11 15:15:18 +0300

    Update users.module.ts

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { User } from './entities/user.entity';
+import { Profile } from '../profile/entities/profile.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([User, Profile]), // Register User & Profile entities
+  ],
   providers: [UsersService],
-  controllers: [UsersController]
+  controllers: [UsersController],
+  exports: [UsersService, TypeOrmModule],
 })
 export class UsersModule {}
